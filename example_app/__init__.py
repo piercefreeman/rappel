802c9e2a3d962fe8c61fa1ad4410d284d0a139fd
sueszli <jabaryyahya@gmail.com>
2026-02-16 23:05:14 +0100

    drop __init__

--- example_app/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Example waymark workflow application."""
-
-from .workflows import ComputationRequest, ComputationResult, ExampleMathWorkflow
-
-__all__ = [
-    "ComputationRequest",
-    "ComputationResult",
-    "ExampleMathWorkflow",
-]
